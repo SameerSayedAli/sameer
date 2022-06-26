2ff57e72e24f795345d4a9b5975fcdcf52315c86
SameerSayedAli <107415798+SameerSayedAli@users.noreply.github.com>
2022-06-26 18:35:47 +0530

    Create GreatestFourNumber.java

---
+++ LabDay2/GreatestFourNumber.java
@@ -0,0 +1,43 @@
+class GreatestFour {
+    
+public static void main(String[] args) {
+        
+int a=20,b=10,c=40,d=30;
+        
+if(a>b)
+{
+if(a>c)
+{
+
+if(a>d)
+{
+  System.out.println(" A is greater number"); 
+}
+else
+{
+   System.out.println("d is greater number"); 
+}
+}
+}
+
+else if(b>c)
+{
+ if(b>d)
+{
+  System.out.println("b is greater number"); 
+}
+else
+{
+System.out.println("d is greater number"); 
+}
+}
+else if(c>d)
+{
+  System.out.println(" c is greater number"); 
+}
+else
+{
+System.out.println("d is greater number"); 
+}
+}
+}
